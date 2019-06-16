1e3118a897d6cd5403808137818ee66f10d13293
Karan Joshi <karanjoshi31@live.com>
2019-06-15 22:05:39 -0400

    added method to return created items

--- models/models.py
+++ models/models.py
@@ -11,6 +11,20 @@ class Item:
         return self.original_price
 
     
+def get_items():
 
+    a = Item("hat", 5.00)
+    b = Item("shirt", 19.45)
+    c = Item("pants", 45.95)
+    d = Item("watch", 205.45)    
+
+    items_list = []
+
+    items_list.append(a)
+    items_list.append(b)
+    items_list.append(c)
+    items_list.append(d)
+
+    return items_list
 
     
